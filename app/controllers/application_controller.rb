4edf960c798e3eea93148b3c393ecd85e99ea5c0
Kierra M <k.merriman32@hotmail.com>
2019-11-14 02:49:19 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,15 @@
 class ApplicationController < Sinatra::Base
 
   configure do
-  	set :views, "app/views"
-  	set :public_dir, "public"
+  	set :views, "app/views" # this tells app where to look for views *our page with HTML to display text in browser 
+  	set :public_dir, "public" #this tells controller where to look for views
   end
 
-  get "/" do
-  	erb :index
+  get "/" do 
+  	erb :index # GET request loads here.. routes
   end
+  #controller action  to accept, request and respond with appropriate html SUCH AS "GET" request
 end
+
+#this is the main controller. other controllers can inherit from here. other times the other controllers will simply inhere from the Sinatra::Base 
+
